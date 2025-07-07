a732ca76c41f4378523e631c29efac070f9e239a
FRA uni <francesca.rossi@upc.edu>
2025-07-07 16:36:43 +0200

    adjust sampling daily profiles for testing

--- Test_daily_profile/sampling_daily_prof.m
+++ Test_daily_profile/sampling_daily_prof.m
@@ -1,6 +1,12 @@
+clear all
+close all
+clc
+
+%% Load Open Data Denmark RES generation and consumption: 24H, every 15 min data for the day 14/07/2020
 open_data_DE=readtable('time_series_15min_singleindex_filtered.csv')
 
-n_loads=4;
+%% Adapt Open Data measures to the test system
+%Generation
 n_gens=3;
 nlhs=height(open_data_DE);
 
@@ -28,24 +34,23 @@ P1_intra_day=P1_day_ahead-2*wind_forecast_std*Pnom_gens(1);
 P1_intra_day(P1_intra_day<0)=0.05*Pnom_gens(1);
 P3_intra_day=P3_day_ahead-2*wind_forecast_std*Pnom_gens(3);
 P3_intra_day(P3_intra_day<0)=0.05*Pnom_gens(3);
+P2_intra_day=P2_day_ahead-0.055*Pnom_gens(2);
+P2_intra_day(P2_intra_day<0)=0.05*Pnom_gens(2);
 
-P2_day_ahead=P2_day_ahead-0.055*Pnom_gens(2);
-P2_day_ahead(P2_day_ahead<0)=0.05*Pnom_gens(2);
+pf_gen_max= 0.95;% power factor (cosphi)
+pf_gen_min= 0.8;% power factor (cosphi)
 
-pf_gen_max= 0.95;
-pf_gen_min= 0.8;
-
-new_samples=lhsdesign(nlhs,n_var);
+new_samples=lhsdesign(nlhs,n_gens);
 pf_gen_arr(:,1)=(pf_gen_max-pf_gen_min)*new_samples(:,1)+pf_gen_min;
 pf_gen_arr(:,2)=(pf_gen_max-pf_gen_min)*new_samples(:,2)+pf_gen_min;
 pf_gen_arr(:,3)=(pf_gen_max-pf_gen_min)*new_samples(:,3)+pf_gen_min;
 
-Q3g_arr=P1_intra_day(:,1).*tan(acos(pf_gen_arr(:,1)));
-Q6g_arr=P2_day_ahead(:,1).*tan(acos(pf_gen_arr(:,2)));
-Q11g_arr=P3_intra_day(:,1).*tan(acos(pf_gen_arr(:,3)));
+Q1_intra_day=P1_intra_day(:,1).*tan(acos(pf_gen_arr(:,1)));
+Q2_intra_day=P2_intra_day(:,1).*tan(acos(pf_gen_arr(:,2)));
+Q3_intra_day=P3_intra_day(:,1).*tan(acos(pf_gen_arr(:,3)));
 
-new_mmc3(:,1)=new_samples(:,4).*(MMC3_max-MMC3_min)+MMC3_min;
-
+%Demand
+n_loads=4;
 isd=[30,20,20,30];
 isd_up=isd*1.3;
 isd_lw=isd*0.7;
@@ -60,42 +65,45 @@ for csd=1:height(open_data_DE)
     stress_dir(csd,:)=loads_stress_dir(csd,:)/sum_loads_sd(csd,1);
 end
 
-Pload_tot_vect_unscaled=open_data_DE.DE_load_actual_entsoe_transparency*1e6;
+Pdemand_unscaled=open_data_DE.DE_load_actual_entsoe_transparency*1e6;
 
-PL_min=200e6;
-PL_max=700e6;
+PL_min=200e6; %minimum total demand
+PL_max=700e6; %maximum totoal demand
 
-Pload_tot_vect_std= (Pload_tot_vect_unscaled-min(Pload_tot_vect_unscaled))/(max(Pload_tot_vect_unscaled)-min(Pload_tot_vect_unscaled));
-Pload_tot_vect=Pload_tot_vect_std*(PL_max-PL_min)+PL_min;
+alpha_Pdemand= (Pdemand_unscaled-min(Pdemand_unscaled))/(max(Pdemand_unscaled)-min(Pdemand_unscaled));
+Pdemand_dayahead=alpha_Pdemand*(PL_max-PL_min)+PL_min;
 
-P2l_arr=-stress_dir(:,1).*Pload_tot_vect;
-P5l_arr=-stress_dir(:,2).*Pload_tot_vect;
-P7l_arr=-stress_dir(:,3).*Pload_tot_vect;
-P9l_arr=-stress_dir(:,4).*Pload_tot_vect;
+PL2_dayahead=-stress_dir(:,1).*Pdemand_dayahead;
+PL5_dayahead=-stress_dir(:,2).*Pdemand_dayahead;
+PL7_dayahead=-stress_dir(:,3).*Pdemand_dayahead;
+PL9_dayahead=-stress_dir(:,4).*Pdemand_dayahead;
 
-pf_loads=0.8;
+pf_loads=0.8; % power factor
 pf_tg_loads=tan(acos(pf_loads));
 
-Q2l_arr=pf_tg_loads*P2l_arr;
-Q5l_arr=pf_tg_loads*P5l_arr;
-Q7l_arr=pf_tg_loads*P7l_arr;
-Q9l_arr=pf_tg_loads*P9l_arr;
+QL2_dayahead=pf_tg_loads*PL2_dayahead;
+QL5_dayahead=pf_tg_loads*PL5_dayahead;
+QL7_dayahead=pf_tg_loads*PL7_dayahead;
+QL9_dayahead=pf_tg_loads*PL9_dayahead;
 
-Pacref_n3_arr=new_mmc3;
+day_ahead_intra_day_forecast=table();
+day_ahead_intra_day_forecast.P1_day_ahead=P1_day_ahead;
+day_ahead_intra_day_forecast.P2_day_ahead=P2_day_ahead;
+day_ahead_intra_day_forecast.P3_day_ahead=P3_day_ahead;
+day_ahead_intra_day_forecast.P1_intra_day=P1_intra_day;
+day_ahead_intra_day_forecast.P2_intra_day=P2_intra_day;
+day_ahead_intra_day_forecast.P3_intra_day=P3_intra_day;
+day_ahead_intra_day_forecast.Q1_intra_day=Q1_intra_day;
+day_ahead_intra_day_forecast.Q2_intra_day=Q2_intra_day;
+day_ahead_intra_day_forecast.Q3_intra_day=Q3_intra_day;
 
-sampling_struct.Pacref_n3_arr=Pacref_n3_arr;
-sampling_struct.P2l_arr=P2l_arr;
-sampling_struct.Q2l_arr=Q2l_arr;
-sampling_struct.P5l_arr=P5l_arr;
-sampling_struct.Q5l_arr=Q5l_arr;
-sampling_struct.P7l_arr=P7l_arr;
-sampling_struct.Q7l_arr=Q7l_arr;
-sampling_struct.P9l_arr=P9l_arr;
-sampling_struct.Q9l_arr=Q9l_arr;
-sampling_struct.P3g_arr=P1_intra_day;
-sampling_struct.Q3g_arr=Q3g_arr;
-sampling_struct.P6g_arr=P2_day_ahead;
-sampling_struct.Q6g_arr=Q6g_arr;
-sampling_struct.P11g_arr=P3_intra_day;
-sampling_struct.Q11g_arr=Q11g_arr;
-%sampling_struct.instance=[1:length(Pload_tot_vect)];
+day_ahead_intra_day_forecast.Pdemand_dayahead=Pdemand_dayahead;
+day_ahead_intra_day_forecast.PL2_dayahead=PL2_dayahead;
+day_ahead_intra_day_forecast.PL5_dayahead=PL5_dayahead;
+day_ahead_intra_day_forecast.PL7_dayahead=PL7_dayahead;
+day_ahead_intra_day_forecast.QL2_dayahead=QL2_dayahead;
+day_ahead_intra_day_forecast.QL5_dayahead=QL5_dayahead;
+day_ahead_intra_day_forecast.QL7_dayahead=QL7_dayahead;
+day_ahead_intra_day_forecast.QL9_dayahead=QL9_dayahead;
+
+writetable(day_ahead_intra_day_forecast,'DayAhead_IntraDay_Forecast.xlsx');
